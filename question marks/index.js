ad3434a11ad1e62e416946ab671968dbea17dba0
Rich Kevan <rskevan@me.com>
2024-03-16 20:56:24 -0700

    Updates to questions marks

--- question marks/index.js
+++ question marks/index.js
@@ -1,8 +1,9 @@
 function QuestionsMarks(str) { 
- 
+  
+
+  // code goes here
   return str;
 }
 
-   
 // keep this function call here 
 module.exports = QuestionsMarks;
--- question marks/solutions/solution_2.js
+++ question marks/solutions/solution_2.js
@@ -3,25 +3,22 @@ function QuestionsMarks(str) {
   str = str.filter((char) => {
     return !isNaN(char) || char === '?';
   });
-  console.log(str);
   counts = [];
   for (let i = 0; i < str.length; i++) {
-    if (!isNaN(str[i])) {
-      for (let j = i+1 ; j < str.length; j++) {
-        if (!isNaN(str[j])) {
-          if (parseInt(str[i]) + parseInt(str[j]) === 10) {
-            if (str.slice(i, j).filter((char) => char === '?').length >= 3) {
-              counts.push(true);
-            } else {
-              counts.push(false);
-            }
+    if (!isNaN(str[i])) {;
+      let section = str.slice(i, i+5);
+      let j = section[4];
+      let total = section.filter((char) => char !== "?").reduce((a,b) => parseInt(a) + parseInt(b), 0);
+      let questionMarks = section.filter((char) => char === '?').length;
+        if (!isNaN(j) && total === 10 && questionMarks >= 3) {
+            counts.push(true);
+          } else if (isNaN(j) && total === 10) {
+            counts.push(false);
           }
         }
       }
+    if (counts.length === 0) {
+      return false;
     }
-  }
-  if (counts.length === 0) {
-    counts.push(false);
-  }
-  console.log(counts.includes(false) ? false : true);
+  return(counts.includes(false) ? false : true);
 }
